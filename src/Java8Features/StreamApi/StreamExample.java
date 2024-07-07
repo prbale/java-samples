c800b5792f399d6f1a6cf2523e1b8582f0fc75e8
Prashant Bale <bale.prashant1984@gmail.com>
2024-07-07 06:47:19 +0530

    Uncommitted changes pushed to server

--- src/Java8Features/StreamApi/StreamExample.java
+++ src/Java8Features/StreamApi/StreamExample.java
@@ -1,2 +1,28 @@
-package Java8Features.StreamApi;public class StreamExample {
+package Java8Features.StreamApi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        List<Employee> employees = Arrays.asList(
+            new Employee("Prashant",  "Engineering", 50000),
+            new Employee("Ajit",  "Admin", 70000),
+            new Employee("Ganesh",  "Engineering", 80000),
+            new Employee("Deepak",  "Audit", 20000),
+            new Employee("Vishal",  "Engineering", 30000)
+        );
+
+
+       List<String> engineeringEmployeeList = employees.stream() // Get Stream
+                .filter(e -> "Engineering".equals(e.getDepartment())) //Filter all inconing employees with Engineering department.
+                .sorted((e1, e2) -> Double.compare(e1.getSalary(), e2.getSalary())) // Sort as per salary
+                .map(Employee::getName) // Fetch only Emplyee names
+                .collect(Collectors.toList()); // Collect to List<String>
+
+
+        System.out.println("List: " + engineeringEmployeeList);
+    }
 }
--- src/Random/Interface/Demo.java
+++ src/Random/Interface/Demo.java
@@ -1,2 +1,26 @@
-package Random.Interface;public class Demo {
+package Random.Interface;
+
+interface MyInterface {
+    // Abstract method
+    void abstractMethod();
+
+    // Default method
+    default void defaultMethod() {
+        System.out.println("This is a default method.");
+    }
+}
+
+class MyClass implements MyInterface {
+    @Override
+    public void abstractMethod() {
+        System.out.println("Abstract method implementation.");
+    }
+}
+
+public class Demo {
+
+    public static void main(String[] args) {
+        MyInterface myClass = new MyClass();
+        myClass.defaultMethod();
+    }
 }
